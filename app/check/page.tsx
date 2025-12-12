bf7e352c925e229f8af120235b293078a61781b9
A R Danish <helloardanish@gmail.com>
2025-12-12 19:28:21 +0530

    testing

--- app/check/page.tsx
+++ app/check/page.tsx
@@ -8,6 +8,11 @@ export default function Check() {
         <Image src="/achhecodelogo.jpg" height={50} width={50} alt="Company Logo" />
         {/* Home */}
         <Button variant="secondary" className="btn-transparent-green">Home</Button>
+
+        <Button variant="secondary" className="btn-transparent">Home
+            {/* <ArrowRight className="group-hover:hidden" /> */}
+            {/* <Check className="hidden group-hover:inline-block" /> */}
+        </Button>
     </div>
   );
 }
